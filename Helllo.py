b08e4d8ba74369bf55c36d04f2c8465c5c433636
Mishika001 <5243@sathyasaiindore.com>
2024-04-09 12:46:46 +0530

    done

---
+++ Helllo.py
@@ -0,0 +1,64 @@
+#  print("hi")
+
+'''price = 1000
+money = 100
+if (price > money):
+    print("I cannot buy pizza")
+else:
+    print("I can buy pizza")'''
+
+'''i = 2
+while i <= 100:
+    print(i)
+    i = i + 2'''
+
+'''i = 100
+while i <= 0:
+    print(i)
+    i = i - 2
+'''
+'''import datetime
+x = datetime.datetime.now()
+print(x.strftime('%A'))'''
+
+'''import datetime
+x = datetime.datetime.now()
+print(x.strftime('%B'))
+'''
+
+
+'''def sum(a, b=0, c=0):
+    k = a + b + c
+    print("sum of a and b and c is =", k)
+
+
+sum(10, 10, 40)
+sum(10)'''
+
+
+'''def mult(a, b, c):
+    k = a * b * c
+    print("multiplication of a and b and c is =", k)
+
+
+mult(10, 10, 10)
+'''
+
+'''def power(a, b):
+    k = a ** b
+    print("exponentiation of a and b and c is =", k)
+
+
+power(10, 10)
+'''
+
+def changelist(list):
+    list.append(10)
+    print(list)
+
+
+list = [1, 2, 3, 4, 5]
+print(list)
+
+changelist(list)
+
