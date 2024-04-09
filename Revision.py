7d0ecd31ebc5de6e7faa2ec2bf835cbd91059800
Mishika001 <5243@sathyasaiindore.com>
2024-04-09 12:42:48 +0530

    done

---
+++ Revision.py
@@ -0,0 +1,131 @@
+'''money = 100
+price = 99
+if (money > price):
+    print("happy")
+else:
+    print("sad")'''
+
+'''i =  100
+while i > 0:
+    if (i % 3 == 0):
+        print(i)
+    i = i - 1'''
+
+'''num1 = float(input("enter the fi:"))
+num2 =float(input("enter the fi:"))
+num3 = float(input("enter the fi:"))
+max_num = num1
+if num2 > max_num:
+    max_num = num2
+if num3 > max_num:
+    max_num = num3
+print(max_num)'''
+
+'''for i in range(-190,190):
+    if(i % 2==0):
+        print(i,"mishika")'''
+
+'''s = "NEW BOOKS"
+for i in s:
+    if i =='K':
+        break
+    print(i)'''
+
+'''import datetime
+x = datetime.datetime.now()
+print(x.strftime('%A'))'''
+
+'''import datetime
+x = datetime.datetime.now()
+print(x.strftime('%B'))'''
+
+'''def sum(a,b):
+    c = a + b
+    print("sum of numbers =",c)
+
+sum(10,20)
+sum(20,30)'''
+
+'''def mult(a,b,c):
+    d = a * b * c
+    print("mult of a and b and c=", d)
+
+mult(10,20,30)'''
+
+'''def sum(a,b=22,c=0):
+    d = a + b + c
+    print(d)
+
+sum(10,10,10)
+sum(11)'''
+
+
+'''def changlist(list):
+    list.append(10)
+    print(list)
+
+
+list = [1,2,4,5,6,7]
+print(list)
+
+
+
+changlist(list)'''
+
+'''a = False
+b = False
+
+x  = a or b
+print(x)'''
+
+'''a = False
+b = True
+
+x  = a and b
+print(x)'''
+
+'''a = False
+b = True
+
+x = not a
+print(x)'''
+
+'''a = 10.1
+b = 20.7
+print(" arithmatic operator a**b is =", a**b)'''
+
+'''x = 10.9
+y = 30.1
+print("comparison operator:")
+print("x!=y =",x!=y)'''
+
+d = {"one":1, "two":2, "three":3}
+
+'''print(d)
+
+n = d["one"]
+print(n)
+
+print(type(d))'''
+
+'''print(d.keys())  # for pycharm (to print all the keys)
+
+print(d.values())  # for pycharm (to print values of all the keys)
+'''
+'''d.values()  # for python idle (to print values of all the keys)'''
+
+
+
+'''print(d.clear())  # for pycharm (to remove all elements in a variable) '''
+
+
+d = {"one": 1, "two": 2, "three": 3}
+
+'''print(d.get("three"))'''  # to print any value of any key
+
+print(d.copy())  # for pycharm (to make copy of dictionary)
+
+
+
+
+
